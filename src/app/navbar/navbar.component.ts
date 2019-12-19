b14b11d09f7bfa4b33533e2e0fece3f874214cfb
Vamsi Mallepalli <vamsi.58c@gmail.com>
2019-12-19 17:41:38 +0530

    menu bar changs

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {TooltipPosition} from '@angular/material';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  
+  
+  public positionOptions: TooltipPosition[] = ['left']; // Position Tool Tip 
+  public position = new FormControl(this.positionOptions[0]); // tslint:disable-next-line:typedef
+  
+  
+  constructor(private authService:AuthService) {
+   }
 
-  constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
